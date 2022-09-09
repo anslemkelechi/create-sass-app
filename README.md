b5a0bbb3926731e4ad4dba127d01b6d295ccac81
Blakcoder <anslemkay180@gmail.com>
2022-09-09 16:03:08 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
+<<<<<<< HEAD
 # create-sass-app
 Get started with using SASS and SCSS on your project easily. The Package follows the 7-1 architecture project structure. 
+=======
+<p align="center"><img src="logo.png" /></p>
+
+<h1 align="center"> Project Title - Create SASS App </h1>
+
+<hr/>
+
+<p>Ever wanted to code up a frontend project with SASS & SCSS and you are stuck with building the acclaimed 7-1 architecture, then you don't need to get stressed about that again, we have abstracted the whole process for you. Get started with using SASS and SCSS on your project easily.</p>
+
+<h3> List of features </h3>
+
+<ul>
+  <li>7-1 Architecture System</li>
+  <li>All partials files automatically created</li>
+  <li>No need creating any <em>File</em> or <em>Folder</em></li>
+  <li>Just Start Building</li>
+</ul>
+
+<h3>Installation & Usage </h3>
+
+```shell
+$ npm i create-sass-app
+```
+
+<h3>Contributing</h3>
+Keep it simple. Keep it minimal. Don't put every single feature just because you can.
+
+<h3>Authors or Acknowledgments</h3>
+<ul>
+  <li>Kelechi Okoronkwo - (Blakcoder)</li>
+</ul>
+
+<h3>License</h3>
+
+This project is licensed under the MIT License
+>>>>>>> 6b67a06 (Initial Commit)
---
+++ create-sass-app.js
@@ -0,0 +1,45 @@
+const fs = require('fs')
+
+const mainDir = `${__dirname}/sass`
+const pathArray = [
+  ['abstracts', ['_variables.scss', '_mixins.scss', '_functions.scss']],
+  [
+    'base',
+    [
+      '_variables.scss',
+      '_reset.scss',
+      '_typography.scss',
+      'animations.scss',
+      '_utilities.scss',
+    ],
+  ],
+  ['components', ['_buttons.scss', '_card.scss', '_form.scss']],
+  ['layouts', ['_navigation.scss', '_header.scss', '_footer.scss']],
+  ['pages', ['_home.scss', '_header.scss', '_footer.scss']],
+  ['themes', ['_pink.scss', '_mint.scss']],
+  ['vendors', ['_icons.scss']],
+]
+
+const createSass = async () => {
+  fs.mkdir(mainDir, { recursive: true }, (err) => {
+    pathArray.forEach((el) => {
+      const folderPath = `${mainDir}/${el[0]}`
+      fs.mkdir(folderPath, { recursive: true }, (err) => {
+        el[1].forEach((cur) => {
+          fs.writeFile(`${folderPath}/${cur}`, 'New File Written', (err) => {
+            console.log('Folders & Files Created')
+          })
+        })
+      })
+    })
+    fs.writeFile(
+      `${mainDir}/main.scss`,
+      'Import all your sass files here',
+      (err) => {
+        console.log('No Error, Start Building')
+      },
+    )
+  })
+}
+
+module.exports = createSass
